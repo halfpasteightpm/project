75d7fda1891c6889d6fe0e76629b5e1baf5aed28
halfpasteightpm <150785462+halfpasteightpm@users.noreply.github.com>
2024-11-19 16:31:10 +0300

    Delete all directory

--- all/selects.sql
+++
@@ -1,7 +0,0 @@
-use restaurantdb1;
-
-select * from customers where firstname like 'i%';
-
-select * from menuitems where price<(select AVG(price) from menuitems);
-
-select * from customers order by bonuspoints desc;
